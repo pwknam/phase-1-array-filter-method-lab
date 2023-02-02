78c893475daf8d944d9c503b830703189f6ba8d8
Kyushik <kyushik95@gmail.com>
2023-02-02 18:27:51 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    const matchingName = drivers.filter(function (driver) {
+        let lowerCaseDriver = driver.toLowerCase();
+        let lowerCaseName = name.toLowerCase();
+        if (lowerCaseDriver === lowerCaseName)
+            return driver
+    })
+    return matchingName
+}
+
+function fuzzyMatch(drivers, letters) {
+    const matchingLetters = drivers.filter(function (driver) {
+        let slicedDriver = driver.slice(0, letters.length)
+        if (slicedDriver === letters) {
+            return driver
+        }
+    })
+    return matchingLetters
+}
+
+function matchName(drivers, argumentName) {
+    const matchingDriver = drivers.filter(function(driver){
+        if (driver.name === argumentName){
+            return driver
+        }
+    })
+    return matchingDriver
+}
